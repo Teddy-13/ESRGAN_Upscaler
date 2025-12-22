3c189f1b5b9f43862f273bfe3022b3d490789d69
Teddy <dragonteddy13@gmail.com>
2025-12-22 23:31:49 +0530

    Initial model commit, without an UI

---
+++ main.py
@@ -0,0 +1,25 @@
+# main.py
+# Example usage of the reusable Real-ESRGAN model
+
+from upscaler import RealESRGANUpscaler
+import random
+
+def main():
+    upscaler = RealESRGANUpscaler(
+        tile=256  # safer for GPUs with limited VRAM
+    )
+
+    num=random.randint(1,10)
+
+    input_image = "input1.jpg"     # put your image here
+    output_image = f"output_{num}.png"
+
+    upscaler.upscale_and_save(
+        input_image,
+        output_image
+    )
+
+    print(f"Upscaled image saved as {output_image}")
+
+if __name__ == "__main__":
+    main()
